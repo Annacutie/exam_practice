e0685d0892db012773469eb07728ea4e4a3778cc
Annacutie <60508805+Annacutie@users.noreply.github.com>
2022-01-30 16:51:41 +0300

    Task1.3 from book sanet.st_How to Code in Python GCSE_ IGCSE_ National 45 and Higher

---
+++ task_1.3.py
@@ -0,0 +1,6 @@
+#1.3
+name=input('Please enter you name: ')
+age=int(input('Please Enter your age: '))
+TV_programe=input('What is your favorite programe? ')
+
+print(f'Listener is {age} years old and likes {TV_programe}')
